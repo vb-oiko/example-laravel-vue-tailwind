1c47fbd0db9bc6dbffac53bad07975b086896848
Vasyl <vasyl@wordproof.com>
2021-02-09 12:40:21 +0200

    add: @apply styling

--- resources/js/views/app.vue
+++ resources/js/views/app.vue
@@ -1,18 +1,24 @@
 <template>
-  <div>
-    {{message}}
-  </div>
+    <div class="message">
+        {{ message }}
+    </div>
 </template>
+
 <script>
 const default_layout = "default";
 
-
 export default {
-  computed: {},
-  data() {
-      return {
-          message:'Hello World'
-      }
-  }
+    computed: {},
+    data() {
+        return {
+            message: "Hello World"
+        };
+    }
 };
 </script>
+
+<style scoped>
+.message {
+    @apply text-blue font-3xl font-bold text-center pt-10;
+}
+</style>
